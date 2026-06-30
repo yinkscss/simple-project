0cbe253732019149bd276b214f761f557b7e2031
yinkscss <olayinkashittu2003@gmail.com>
2026-06-30 14:04:39 +0100

    Simple calculator

--- report.js
+++ report.js
@@ -7,4 +7,27 @@ if (girl == boy) {
 } else {
     console.log("false gender selected.");
 
-} 
+}
+
+
+var x = 10;
+var y = 11;
+
+var operator = "add"
+
+function calculate(x, y, operator) {
+    if (operator == "add") {
+        return x + y;
+    } else if (operator == "subtract") {
+        return x - y;
+    } else if (operator == "multiply") {
+        return x * y;
+    } else if (operator == "divide") {
+        return x / y;
+    }
+
+
+}
+
+
+console.log(calculate(x, y, operator));
